66978a90e1a695074049172159aeb960e2054728
KareyZhang <KareyZhang@gmail.com>
2011-06-13 03:00:00 +0000

    引入averaged 模型可保存 输出可保存 评价可显示p、r、F1值

---  perminusminus/path_labeling/core.h
+++  perminusminus/path_labeling/core.h
@@ -4,6 +4,7 @@
 #include<stdio.h>
 #include "graph_file.h"
 /*the structure of the model, also the file of the model*/
+const int DEC=1000;
 struct Model{
     int l_size;//size of the labels
     int f_size;//size of the features
@@ -13,18 +14,29 @@ struct Model{
     double* ave_ll_weights;
     double* ave_fl_weights;//weights of (feature,label)
     
-    int is_learning;
-    Model(int l,int f,int is_learning=0){
+    
+    Model(int l,int f){
         this->l_size=l;
         this->f_size=f;
         this->ll_weights=(int*)calloc(sizeof(int),l*l);
         this->fl_weights=(int*)calloc(sizeof(int),l*f);
-        if(is_learning){
-            this->ave_ll_weights=(double*)calloc(sizeof(double),l*l);
-            this->ave_fl_weights=(double*)calloc(sizeof(double),l*f);
-        }
-        this->is_learning=is_learning;
+        this->ave_ll_weights=(double*)calloc(sizeof(double),l*l);
+        this->ave_fl_weights=(double*)calloc(sizeof(double),l*f);
     }
+    
+    void average(int step){
+        int l_size=this->l_size;
+        int f_size=this->f_size;
+        for(int i=0;i<l_size*f_size;i++){
+            this->fl_weights[i]=(int)(((double)((this->fl_weights[i])*step-
+                    (this->ave_fl_weights[i]))/step+0.5)*DEC);
+        }
+        for(int i=0;i<l_size*l_size;i++){
+            this->ll_weights[i]=(int)(((double)((this->ll_weights[i])*step-
+                    (this->ave_ll_weights[i]))/step+0.5)*DEC);
+        }
+    }
+    
     Model(char* filename){
         FILE* pFile;
         pFile=fopen(filename,"rb");
@@ -38,11 +50,25 @@ struct Model{
         
         fread((this->ll_weights),4,l_size*l_size,pFile);
         fread((this->fl_weights),4,l_size*f_size,pFile);
+        fclose(pFile);
+    }
+    
+    void save(char* filename){
+        FILE* pFile=fopen(filename,"wb");
+        int l_size=this->l_size;
+        int f_size=this->f_size;
+        
+        fwrite(&(this->l_size),4,1,pFile);
+        fwrite(&(this->f_size),4,1,pFile);
+        printf("%d %d\n",l_size,f_size);
+        fwrite((this->ll_weights),4,l_size*l_size,pFile);
+        fwrite((this->fl_weights),4,l_size*f_size,pFile);
+        fclose(pFile);
     }
     ~Model(){
         free(this->ll_weights);
         free(this->fl_weights);
-        if(this->is_learning){
+        if((this->ave_ll_weights)!=NULL){
             free(this->ave_ll_weights);
             free(this->ave_fl_weights);
         }
@@ -52,8 +78,4 @@ struct Model{
 
 
 
-void save_model(Model* model,char* filename){
-};
-void load_model(char* filename){
-};
 #endif
