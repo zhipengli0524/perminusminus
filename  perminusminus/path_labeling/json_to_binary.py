43b5b848cba5ec23675e69a9641f6f6895faabaf
KareyZhang <KareyZhang@gmail.com>
2011-06-13 06:33:01 +0000

    fix the format of the binary file and json file. btw: we has test it for CWS. The F1 for sighan bakeoff 2005 msr corpus is 0.9644

---
+++  perminusminus/path_labeling/json_to_binary.py
@@ -0,0 +1,53 @@
+import json
+import array
+import struct
+
+'''
+
+type:
+    0: mormal
+    1: initial
+    2: terminal
+    3: initial and terminal
+
+format of the json file:
+each line is a graph object
+graph::= [node, ... ]
+node ::= [type,[predecessor, ...],label,[feature, ...]]
+
+format for each graph
+remaining_int_length node_count
+(node type)+ (predecessors -1)+ (label)+ (features -1)+
+'''
+
+def transform(json_file,bin_file):
+    sen=0
+    binfile=open(bin_file,'wb')
+    for line in open(json_file,encoding='utf8'):
+        sen+=1
+        graph=json.loads(line)
+        node_count=len(graph)
+        node_types=[n[0] for n in graph]
+        labels=[n[2] for n in graph]
+        #print(labels)
+        pre=[]
+        features=[]
+        for ind,node in enumerate(graph):
+            pre.extend([x for x in node[1]])
+            pre.append(-1)
+            features.extend(node[3])
+            features.append(-1)
+        arr=array.array('i')
+        data=[node_count]+node_types+pre+labels+features
+        arr.fromlist([(len(data))]+data)
+        arr.tofile(binfile)
+        continue
+    binfile.close()
+    print("finished",sen,"sentences")
+
+if __name__=='__main__':
+
+    #transform("toy.json","toy.bin")
+    #transform("test.json","test.bin")
+    transform("training.json","training.bin")
+    
