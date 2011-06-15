9930421d1e079ac41e6fa7619f97aa5d0e3770aa
KareyZhang@gmail.com <KareyZhang@gmail.com@f0e2b59c-80d6-e102-5d19-adf5c1f436e9>
2011-06-15 12:17:21 +0000

    a CWS example

---
+++ trunk/ perminusminus/CWS_example/cws_transform.py
@@ -0,0 +1,107 @@
+import json
+import sys
+import json_to_binary
+'''
+将msr的训练集、测试集特征化序列化，变成工具包可以处理的样子
+'''
+
+class FeatureIndexer(dict):
+    def __init__(self):
+        pass
+    def __call__(self,keys):
+        return [self.setdefault(k,len(self))for k in keys]
+    def test(self,keys):
+        return [self[k] for k in keys if k in self]
+        
+def _to_tags(sen):
+    tags=[]
+    for w in sen:
+        if len(w)==1:
+            tags.append(3)
+        else:
+            tags.append(0)
+            for i in range(len(w)-2):tags.append(1)
+            tags.append(2)
+    return tags
+
+
+def gen_keys(seq,i):
+    mid=seq[i]
+    left=seq[i-1] if i>0 else '#'
+    left2=seq[i-2] if i-1>0 else '#'
+    right=seq[i+1] if i+1<len(seq) else '#'
+    right2=seq[i+2] if i+2<len(seq) else '#'
+    return [mid,'l:'+left,'r:'+right,
+            'l:'+left+mid,'r:'+mid+right,
+            'l2:'+left2+left,'r2:'+right+right2]
+
+def train(src,dst,index):
+    fid=FeatureIndexer()
+    file=open(dst,'wb')
+    for line in open(src,encoding='utf8'):
+        line=line.split()
+        seq=''.join(line)
+        graph=[]
+        fs=[fid(gen_keys(seq,x)) for x in range(len(seq))]
+        for c,v in zip(_to_tags(line),fs):
+            graph.append([0,[],c,v])
+        if not graph:continue
+        graph[0][0]+=1;
+        graph[-1][0]+=2;
+        for i in range(1,len(graph)):
+            graph[i][1]=[i-1]
+        #print(json.dumps(graph),file=file)
+        json_to_binary.graph_to_file(graph,file)
+    
+    file.close()
+    print(len(set(fid.values())))
+    print(len(fid))
+    
+    
+    file=open(index,'w',encoding='utf8')
+    json.dump(dict(fid),file,ensure_ascii=False,indent=1)
+    file.close()
+    print('the end')
+
+def test(index,src,dst):
+    file=open(index,encoding='utf8')
+    fid=FeatureIndexer()
+    fid.update(json.load(file))
+    file.close()
+    file=open(dst,'wb')
+    for line in open(src,encoding='utf8'):
+        line=line.split()
+        seq=''.join(line)
+        graph=[]
+        fs=[fid.test(gen_keys(seq,x)) for x in range(len(seq))]
+        for c,v in zip(_to_tags(line),fs):
+            graph.append([0,[],c,v])
+        if not graph:continue
+        graph[0][0]+=1;
+        graph[-1][0]+=2;
+        for i in range(1,len(graph)):
+            graph[i][1]=[i-1]
+        #print(json.dumps(graph),file=file)
+        json_to_binary.graph_to_file(graph,file)
+    print('the end')
+    file.close()
+
+if __name__=='__main__':
+    if len(sys.argv)==1:#empty argv, do the debug
+        print("reading training data")
+        train('msr_training.utf8','training.bin','index.json')
+        print("reading test data")
+        test('index.json','msr_test_gold.utf8','test.bin')
+        exit()
+    
+    task=sys.argv[1]
+    if task=='l':
+        print("reading training data")
+        print((sys.argv[2],sys.argv[3],sys.argv[4]))
+        train(sys.argv[2],sys.argv[3],sys.argv[4])
+        exit()
+    if task=='p':
+        print("reading test data")
+        test(sys.argv[2],sys.argv[3],sys.argv[4])
+        exit()
+    
--- trunk/ perminusminus/CWS_example/json_to_binary.py
+++ trunk/ perminusminus/CWS_example/json_to_binary.py
@@ -20,28 +20,32 @@ remaining_int_length node_count
 (node type)+ (predecessors -1)+ (label)+ (features -1)+
 '''
 
+def graph_to_file(graph,binfile):
+    
+    node_count=len(graph)
+    node_types=[n[0] for n in graph]
+    labels=[n[2] for n in graph]
+    #print(labels)
+    pre=[]
+    features=[]
+    for ind,node in enumerate(graph):
+        pre.extend([x for x in node[1]])
+        pre.append(-1)
+        features.extend(node[3])
+        features.append(-1)
+    arr=array.array('i')
+    data=[node_count]+node_types+pre+labels+features
+    arr.fromlist([(len(data))]+data)
+    arr.tofile(binfile)
+    
+
 def transform(json_file,bin_file):
     sen=0
     binfile=open(bin_file,'wb')
     for line in open(json_file,encoding='utf8'):
         sen+=1
         graph=json.loads(line)
-        node_count=len(graph)
-        node_types=[n[0] for n in graph]
-        labels=[n[2] for n in graph]
-        #print(labels)
-        pre=[]
-        features=[]
-        for ind,node in enumerate(graph):
-            pre.extend([x for x in node[1]])
-            pre.append(-1)
-            features.extend(node[3])
-            features.append(-1)
-        arr=array.array('i')
-        data=[node_count]+node_types+pre+labels+features
-        arr.fromlist([(len(data))]+data)
-        arr.tofile(binfile)
-        continue
+        graph_to_file(graph,bin_file)
     binfile.close()
     print("finished",sen,"sentences")
 
