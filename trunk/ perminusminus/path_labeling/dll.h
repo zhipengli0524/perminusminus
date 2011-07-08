534377d08157620af54ca9b3d6df0525ec149117
KareyZhang@gmail.com <KareyZhang@gmail.com@f0e2b59c-80d6-e102-5d19-adf5c1f436e9>
2011-07-08 03:36:37 +0000

    api for both Win and Linux

--- trunk/ perminusminus/path_labeling/dll.h
+++ trunk/ perminusminus/path_labeling/dll.h
@@ -1,8 +1,16 @@
 /* FILE dll.h */
+
+
+#ifdef WIN32
+
 #ifdef BUILD_DLL
 /* DLL export */
 #define EXPORT __declspec(dllexport)
 #else
 /* EXE import */
 #define EXPORT __declspec(dllimport)
+#endif
+
+#else
+#define EXPORT
 #endif
