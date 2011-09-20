27a155f3e03a1d2b3f245c4f8c7af491ef36d526
KareyZhang@gmail.com <KareyZhang@gmail.com@f0e2b59c-80d6-e102-5d19-adf5c1f436e9>
2011-09-20 01:39:34 +0000

    准备修改bug：tagging的时候alpha+beta-value最大值不相等

--- trunk/ perminusminus/tagging/train_and_test.sh
+++ trunk/ perminusminus/tagging/train_and_test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-../path_labeling/per-- l "data/training.bin" "data/model.bin" 25
-../path_labeling/per-- p "data/model.bin" "data/test.bin" "data/result.txt" -2
+
+../path_labeling/per-- l "data/training.bin" "data/model.bin" 15
+../path_labeling/per-- p "data/model.bin" "data/test.bin" "data/result.txt" 0
+
+
+#~/exp/wheels/dat/c_dat.py data/index.txt data/index.bin
+#../sl_decoder/sl_decoder data/model.bin data/index.bin data/label_info.txt <data/ctb_test_raw.txt > data/ctb_test_result.txt
+
