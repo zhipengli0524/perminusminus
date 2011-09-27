a75f9dcf38636d65458990fc343a2ee0f8e66ecf
KareyZhang@gmail.com <KareyZhang@gmail.com@f0e2b59c-80d6-e102-5d19-adf5c1f436e9>
2011-09-27 08:27:31 +0000

    做了一些小优化

--- trunk/ perminusminus/path_labeling/decoder.h
+++ trunk/ perminusminus/path_labeling/decoder.h
@@ -129,34 +129,40 @@ inline void dp_decode(
     int* p_pre_label;
     int* p_allowed_label;
     int k;
+    int j;
     Alpha_Beta* tmp;
     Alpha_Beta best;best.node_id=-1;
+    Alpha_Beta* pre_alpha;
     int score;
-    //if(allowed_label_lists)printf("haha");
+    
+    for(int i=0;i<node_count*l_size;i++)alphas[i].node_id=-2;
     for(int i=0;i<node_count;i++){//for each node
         p_allowed_label=allowed_label_lists?allowed_label_lists[i]:NULL;
         
-        for(int j=0;j<l_size;j++){///遍历当前节点所有可能的标签
+        j=-1;
+        while((p_allowed_label?
+                    ((j=(*(p_allowed_label++)))!=-1):
+                    ((++j)!=l_size))){
             tmp=&alphas[i*l_size+j];
-            tmp->node_id=-2;//non reachable
-            
-            if(p_allowed_label){
-                if(((*p_allowed_label)!=-1)&&((*p_allowed_label)!=j))
-                    continue;
-                p_allowed_label++;
-            }
             tmp->value=0;
             p_node_id=nodes[i].predecessors;
             p_pre_label=pre_labels?pre_labels[j]:NULL;
             while((node_id=*(p_node_id++))>=0){
                 if(p_pre_label){
                     while((k=(*p_pre_label++))!=-1){
-                        if(alphas[node_id*l_size+k].node_id==-2)continue;//not reachable
-                        score=alphas[node_id*l_size+k].value+ll_weights[k*l_size+j];
-                        if((tmp->node_id<0)||(score>tmp->value)){
-                            tmp->value=score;
+                        pre_alpha=alphas+node_id*l_size+k;
+                        if(pre_alpha->node_id==-2)continue;//not reachable
+                        if((tmp->node_id<0)){
+                            tmp->value=pre_alpha->value+ll_weights[k*l_size+j];
                             tmp->node_id=node_id;
                             tmp->label_id=k;
+                        }else{
+                            score=pre_alpha->value+ll_weights[k*l_size+j];
+                            if(score>tmp->value){
+                                tmp->value=score;
+                                tmp->node_id=node_id;
+                                tmp->label_id=k;
+                            }
                         }
                     }
                 }else{
