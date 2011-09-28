dff096cdf9d6b73a50b28fd20ca80c15a6f2316c
KareyZhang <KareyZhang@gmail.com>
2011-09-28 09:49:30 +0000

    cws_*可以统一到tagging_*下面了。以后也不需要cws_*了

---  perminusminus/path_labeling/command_line.cpp
+++  perminusminus/path_labeling/command_line.cpp
@@ -24,9 +24,16 @@ int main(int argc, char **argv){
     
     int c;
     char* allowed_label_bigram_file=NULL;
+    int output_config=0;
     //char* label_lists_file=NULL;
-    while ( (c = getopt(argc, argv, "b:h")) != -1) {
+    while ( (c = getopt(argc, argv, "12b:h")) != -1) {
         switch (c) {
+            case '1' : 
+                output_config=-1;
+                break;
+            case '2' : 
+                output_config=-2;
+                break;
             case 'b' : 
                 allowed_label_bigram_file=optarg;
                 break;
@@ -48,10 +55,13 @@ int main(int argc, char **argv){
                 argv[optind+2],
                 strtol(argv[optind+3],&strend,10),allowed_label_bigram_file);
     }else if(argv[optind][0]=='p'){
+        if(optind+4<argc){
+            output_config=strtol(argv[optind+4],&strend,10);
+        }
         test(argv[optind+1],
              argv[optind+2],
              argv[optind+3],
-             strtol(argv[optind+4],&strend,10),
+             output_config,
              allowed_label_bigram_file);
         
     }
