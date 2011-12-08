5b57f64414f45efc0b18f806ef22eb735d93cb93
KareyZhang <KareyZhang@gmail.com>
2011-12-08 06:11:17 +0000

    加入词表成功，分词和词性标注效果均有提高。 目前只实现了py部分。c++解码程序以后再说。

---  perminusminus/tagging/train_and_test.sh
+++  perminusminus/tagging/train_and_test.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-../path_labeling/per-- l "data/training.bin" "data/model.bin" 15
-../path_labeling/per-- p "data/model.bin" "data/test.bin" "tmp/result.txt" -1
+##transform
+#./transform.py -l -s \_  -d slex.txt data/training.seg tmp/training.bin tmp/index.txt tmp/label_index.txt
+#./transform.py -p -s \_  -d slex.txt data/test.seg tmp/test.bin tmp/index.txt tmp/label_index.txt
 
+#./transform.py -l -s \   data/training.seg tmp/training.bin tmp/index.txt tmp/label_index.txt
+#./transform.py -p -s \  data/test.seg tmp/test.bin tmp/index.txt tmp/label_index.txt
+            
+
+
+./transform.py -l -d slex.txt data/training.tag tmp/training.bin tmp/index.txt tmp/label_index.txt
+./transform.py -p -d slex.txt data/test.tag tmp/test.bin tmp/index.txt tmp/label_index.txt
+
+../path_labeling/per-- l "tmp/training.bin" "tmp/model.bin" 15
+../path_labeling/per-- p "tmp/model.bin" "tmp/test.bin" "tmp/result.txt" -1
+
+#evaluation
+./tagging_eval.py
 
 #~/exp/wheels/dat/c_dat.py data/index.txt data/index.bin
 #../sl_decoder/sl_decoder data/model.bin data/index.bin data/label_info.txt <data/ctb_test_raw.txt > data/ctb_test_result.txt
